3c766f63d548e1f63177e93a83d86f5ca0d0ed39
Wang Chao <wangchao2423@gmail.com>
2024-09-10 16:47:16 +0800

    feat: 基本功能实现

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 import { basekit, FieldType, field, FieldComponent, FieldCode } from '@lark-opdev/block-basekit-server-api';
 import { searchCardBin } from 'bankcard';
 
+import AddressParse from './utils/address/parse/index';
+
 const { t } = field;
 
 // 通过addDomainList添加请求接口的域名
@@ -15,20 +17,41 @@ basekit.addField({
       'zh-CN': {
         source: '选择待解析的字段',
         p1: '请选择文本类型字段',
-        bankName: '银行名字',
-        cardType: '卡种',
+        p2: '收件人',
+        p3: '联系电话',
+        p4: '省市区',
+        p5: '详细地址',
+        p6: '省份',
+        p7: '城市',
+        p8: '区县',
+        p9: '邮编',
+        p10: '省市区编号',
       },
       'en-US': {
-        source: 'Select the field to convert',
-        p1: 'Please select a text field.',
-        bankName: 'Bank Name',
-        cardType: 'Card Type',
+        source: 'Select the field to parse',
+        p1: 'Please select a text field',
+        p2: 'Recipient',
+        p3: 'Contact number',
+        p4: 'Province/City/District',
+        p5: 'Detailed address',
+        p6: 'Province',
+        p7: 'City',
+        p8: 'District/County',
+        p9: 'Postal code',
+        p10: 'Province/City/District code',
       },
       'ja-JP': {
-        source: '変換するフィールドを選択',
-        p1: 'テキストタイプのフィールドを選択',
-        bankName: '銀行名',
-        cardType: 'カード種別',
+        source: '解析するフィールドを選択してください',
+        p1: 'テキストフィールドを選択してください',
+        p2: '受取人',
+        p3: '連絡先番号',
+        p4: '都道府県/市区町村',
+        p5: '詳細住所',
+        p6: '都道府県',
+        p7: '市',
+        p8: '区/郡',
+        p9: '郵便番号',
+        p10: '都道府県/市区町村コード',
       },
     },
   },
@@ -56,16 +79,51 @@ basekit.addField({
       },
       properties: [
         {
-          key: 'bankName',
+          key: 'person',
           primary: true,
           isGroupByKey: true,
           type: FieldType.Text,
-          title: t('bankName'),
+          title: t('p2'),
         },
         {
-          key: 'cardType',
+          key: 'phone',
           type: FieldType.Text,
-          title: t('cardType'),
+          title: t('p3'),
+        },
+        {
+          key: 'area',
+          type: FieldType.Text,
+          title: t('p4'),
+        },
+        {
+          key: 'address',
+          type: FieldType.Text,
+          title: t('p5'),
+        },
+        {
+          key: 'province',
+          type: FieldType.Text,
+          title: t('p6'),
+        },
+        {
+          key: 'city',
+          type: FieldType.Text,
+          title: t('p7'),
+        },
+        {
+          key: 'district',
+          type: FieldType.Text,
+          title: t('p8'),
+        },
+        {
+          key: 'postCode',
+          type: FieldType.Text,
+          title: t('p9'),
+        },
+        {
+          key: 'areaCode',
+          type: FieldType.Text,
+          title: t('p10'),
         },
       ],
     },
@@ -76,15 +134,27 @@ basekit.addField({
 
     // 数字类型 source 直接为值
     //  文本类型 source 为 [{ type: 'text , text '8'}]
-    const sourceValue = Array.isArray(source) && source.length > 0 && source[0].text.split(' ').join('');
-    const res = await searchCardBin(sourceValue as string);
+    const valueStr = Array.isArray(source) && source.length > 0 && source[0].text.split(' ').join('');
+    const [result] = AddressParse.parse(valueStr);
+
+    let phoneStr = result.mobile || result.phone;
+    if (result.phone && result.mobile) {
+      phoneStr = [result.mobile, result.phone].join(',');
+    }
 
     try {
       return {
         code: FieldCode.Success,
         data: {
-          bankName: res.bankName,
-          cardType: res.cardTypeName,
+          person: result.name,
+          phone: phoneStr,
+          area: `${result.province}${result.city}${result.area}`,
+          address: result.details,
+          postCode: result.zip_code,
+          areaCode: result.code,
+          province: result.province,
+          city: result.city,
+          district: result.area,
         },
       };
     } catch (e) {
